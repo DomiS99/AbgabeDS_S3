ae01ac369f678e5baa010732616e1f844cd15cb6
Dominique Schwaiger <dominiqueschwaiger@gmx.at>
2022-07-14 20:26:27 +0200

    Update1407|2026

--- MyCompany.MathProgram/MyCompany.MathProgram/ServiceInterface/IMathService.cs
+++ MyCompany.MathProgram/MyCompany.MathProgram/ServiceInterface/IMathService.cs
@@ -2,10 +2,9 @@
 
 namespace MyCompany.MathProgram.ServiceInterface
 {
-    public interface IMathService
+    public interface IMathService //nur übergeben was Übergeben wird und was zurückkommen soll, nie wie es gemacht wird
     {
         ResponseDTO Sum(RequestDTO requestDTO); //Gibt immer eine Klasse ResponseDTO zuück und muss ein RequestDTO übergeben bekommen
-
         ResponseDTO Sub(RequestDTO requestDTO);
         ResponseDTO Mult(RequestDTO requestDTO);
     }
